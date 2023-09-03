4ab66bec0eda18e55c1d749e57930fc0001806b6
artur <gorski.artur1@gmail.com>
2023-09-03 23:38:49 +0200

    add index controller to shop route

--- routes/shop.ts
+++ routes/shop.ts
@@ -3,13 +3,15 @@ import shopController from "../controllers/shopController";
 
 export const shopRoutes = express.Router();
 
-shopRoutes.get("/", shopController.getProducts);
+shopRoutes.get("/", shopController.getIndex);
 
 shopRoutes.get("/products", shopController.getProducts);
 
 shopRoutes.get("/products/:productId", shopController.getProduct);
 
 shopRoutes.get("/cart", shopController.getCart);
+
+shopRoutes.post("/cart", shopController.postCart);
 
 shopRoutes.get("/orders", shopController.getOrders);
 
